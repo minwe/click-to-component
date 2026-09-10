34d0910b198465983b755c087f3d7eeea478a516
Minwe LUO <minwe@users.noreply.github.com>
2026-09-10 11:42:35 +0800

    feat: support IntelliJ IDE (IDEA and WebStorm)
    - Add protocol detection for IntelliJ editors (IDEA, WebStorm)
    - Use different protocol format for IntelliJ: `editor://open?file` instead of `editor://file`

---
+++ .changeset/intellij-ide-support.md
@@ -0,0 +1,5 @@
+---
+"click-to-react-component": minor
+---
+
+Add support for opening components in IntelliJ IDEA and WebStorm.
--- packages/click-to-react-component/src/getUrl.js
+++ packages/click-to-react-component/src/getUrl.js
@@ -4,10 +4,16 @@
  * @param {string} param.pathToSource
  */
 export function getUrl({ editor, pathToSource }) {
+  // For IntelliJ IDEA and WebStorm, use different protocol
+  const intelliJEditors = ['idea', 'webstorm']
+  const isIntelliJ = intelliJEditors.includes(editor)
+  // https://youtrack.jetbrains.com/articles/SUPPORT-A-2159/How-to-open-a-source-file-with-line-and-column-specified-from-an-URL
+  const protocol = isIntelliJ ? `${editor}://open?file` : `${editor}://file`
+
   // Fix https://github.com/microsoft/vscode/issues/197319
   if (pathToSource[0] === '/') {
-    return `${editor}://file${pathToSource}`
+    return `${protocol}${pathToSource}`
   }
 
-  return `${editor}://file/${pathToSource}`
+  return `${protocol}/${pathToSource}`
 }
