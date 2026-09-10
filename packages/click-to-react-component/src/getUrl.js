45460850eb0b885b282a1848b05826b9dd76c58a
Minwe LUO <minwe@users.noreply.github.com>
2026-09-10 11:42:35 +0800

    refactor: use array to check IntelliJ editors for better extensibility

--- packages/click-to-react-component/src/getUrl.js
+++ packages/click-to-react-component/src/getUrl.js
@@ -4,10 +4,15 @@
  * @param {string} param.pathToSource
  */
 export function getUrl({ editor, pathToSource }) {
+  // For IntelliJ IDEA and WebStorm, use different protocol
+  const intelliJEditors = ['idea', 'webstorm']
+  const isIntelliJ = intelliJEditors.includes(editor)
+  const protocol = isIntelliJ ? `${editor}://open?file` : `${editor}://file`
+
   // Fix https://github.com/microsoft/vscode/issues/197319
   if (pathToSource[0] === '/') {
-    return `${editor}://file${pathToSource}`
+    return `${protocol}${pathToSource}`
   }
 
-  return `${editor}://file/${pathToSource}`
+  return `${protocol}/${pathToSource}`
 }
